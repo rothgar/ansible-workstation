f61cc2c3bb6295756481639abe7e5f7e86874602
Justin Garrison <justinleegarrison@gmail.com>
2015-06-05 16:26:45 -0700

    added $CLONE_DIR and disabled cows :cow:

--- init.sh
+++ init.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 REPO="git@github.com:rothgar/ansible-workstation.git"
+CLONE_DIR=/home/"${SUDO_USER}"/src/ansible-workstation
 
 # install initial packages
 yum install -y ansible git zsh
 
 # clone this repo
-mkdir -p /home/"${SUDO_USER}/src
-git clone "${REPO}" /home/"${SUDO_USER}"/src/ansible-workstation 
+mkdir -p "${CLONE_DIR}"
+git clone "${REPO}" "${CLONE_DIR}"
 
-ansible-playbook /home/"${SUDO_USER}"/src/ansible-workstation/main.yml
+ANSIBLE_NOCOWS=1 ansible-playbook -u ${SUDO_USER} -i "${CLONE_DIR}"/hosts.template "${CLONE_DIR}"/main.yml
